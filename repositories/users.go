152f172c6e1322663fa377aaad8a09fa2a50b14a
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-08 11:16:31 +0700

    Delete data with ORM

--- repositories/users.go
+++ repositories/users.go
@@ -49,7 +49,7 @@ func (r *repository) UpdateUser(user models.User) (models.User, error) {
 }
 
 func (r *repository) DeleteUser(user models.User) (models.User, error) {
-	err := r.db.Delete(&user).Error
+	// Using "Delete" method here ...
 
 	return user, err
 }
